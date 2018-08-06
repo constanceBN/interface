a4d45f463906be8c869fca0170136c32ba3bf11c
baghera <baghera@192.168.1.107>
2018-08-06 16:00:14 +0200

    V2 (dans l'interface, rajout des getters  et setters communs aux deux méthodes).

--- src/ex1/ExempleInterface.java
+++ src/ex1/ExempleInterface.java
@@ -4,5 +4,9 @@ public interface ExempleInterface {
 	
 	public int minimum();
 	public int maximum();
+	
+	// Dans l'interface, on met les signatures des getters et setters communs à nos deux méthodes.
+	public int getV1();
+	public int getV2();
 
 }
